82bc5d182e8ba8ddb58f6e94be859c59390e6fc4
Tito590 <cenditito@gmail.com>
2026-05-12 16:07:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,31 @@
+self.addEventListener('push', function(event) {
+    // Digunakan jika nanti pakai server asli
+});
+
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     event.waitUntil(
-        clients.openWindow('https://tito590.github.io/jogrestech/testnotifikasi.html')
+        clients.openWindow('/')
     );
 });
+
+// Fungsi untuk memastikan notifikasi muncul dengan prioritas tinggi
+self.onmessage = (event) => {
+    if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
+        const title = "GOL!! ⚽";
+        const options = {
+            body: "Messi 78'\nArgentina 2 - 1 Prancis",
+            icon: "https://flagcdn.com/w160/ar.png",
+            badge: "https://flagcdn.com/w96/ar.png",
+            vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40],
+            data: { dateOfArrival: Date.now() },
+            // Ini kunci utamanya:
+            tag: "goal-alert",
+            renotify: true,
+            requireInteraction: true,
+            // Menambah level kepentingan
+            priority: 2 // High Priority
+        };
+        self.registration.showNotification(title, options);
+    }
+};
