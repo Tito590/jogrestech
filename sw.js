59827d7307aeb48256d3199aab8e94d4a698e3e0
Tito590 <cenditito@gmail.com>
2026-05-12 18:48:43 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,13 @@
-// Service Worker untuk menangani interaksi notifikasi
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    
     event.waitUntil(
         clients.matchAll({type: 'window'}).then(function(clientList) {
-            // Fokus ke tab yang sudah terbuka jika ada
-            for (var i = 0; i < clientList.length; i++) {
-                var client = clientList[i];
-                if ('focus' in client) return client.focus();
-            }
-            // Jika tidak ada, buka halaman baru
-            if (clients.openWindow) return clients.openWindow('./testnotifikasi.html');
+            if (clientList.length > 0) return clientList[0].focus();
+            return clients.openWindow('./testnotifikasi.html');
         })
     );
 });
 
-// Memastikan Service Worker segera aktif
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
     self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(clients.claim());
 });
