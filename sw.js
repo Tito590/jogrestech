f2a9c3e566ce171d5e566e89dee663885b64e7ed
Tito590 <cenditito@gmail.com>
2026-05-12 20:10:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,17 @@
-// Menangani klik pada notifikasi
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({type: 'window'}).then(function(clientList) {
-            // Jika aplikasi sudah terbuka, fokuskan saja
-            for (var i = 0; i < clientList.length; i++) {
-                var client = clientList[i];
-                if ('focus' in client) return client.focus();
-            }
-            // Jika tidak, buka halaman baru
-            if (clients.openWindow) return clients.openWindow('./testnotifikasi.html');
-        })
-    );
-});
-
-// Memastikan update service worker segera aktif
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({type: 'window', includeUncontrolled: true}).then(function(clientList) {
+            if (clientList.length > 0) return clientList[0].focus();
+            return clients.openWindow('./testnotifikasi.html');
+        })
+    );
 });
