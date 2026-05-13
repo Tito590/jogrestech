ff6cb26020708ef0ebd6387edc2959078af078fd
Tito590 <cenditito@gmail.com>
2026-05-13 08:49:34 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,20 @@
+// Gunakan versi 8.10.1 yang stabil
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: "208387659179" // Sender ID Anda
+    messagingSenderId: "208387659179" // Sender ID kamu sudah benar
 });
 
 const messaging = firebase.messaging();
 
-// Menangkap notifikasi saat aplikasi di background/tutup
+// Background handler
 messaging.onBackgroundMessage((payload) => {
-    console.log('Notifikasi masuk di background:', payload);
+    console.log('Pesan Background:', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: 'https://flagcdn.com/w160/ar.png',
-        badge: 'https://flagcdn.com/w96/ar.png',
+        icon: 'https://flagcdn.com/w160/ar.png'
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
